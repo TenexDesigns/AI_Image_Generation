415ae654db5d02533d73814f54ca7679f262639e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-01 09:08:31 +0300

    Create 1_AI_Image_Generators.rb

---
+++ 1_AI_Image_Generators.rb
@@ -0,0 +1,14 @@
+playgroundai.com  --- 1000 free images per day
+dezgo.com/  -- free image genaration
+leonardo.ai  --- you can use it to generate images but at 150 tokens, you can also prompt generate here
+https://www.thomas.io/stable-diffusion-prompt-generator?ref=theresanaiforthat ----  for prompt generation
+  https://theresanaiforthat.com/  --- Find ai too for what ever you want
+
+
+
+
+
+
+
+
+
